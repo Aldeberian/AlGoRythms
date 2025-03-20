eadc04f6e3b640d7340f75bbbef6b5160fb4735f
cleo.eiras <cleo.eiras@etu.uca.fr>
2025-03-20 05:41:43 +0100

    structure

---
+++ leetcode_problems/tree/94_Binary_Tree_Inorder_Traversal.go
@@ -0,0 +1,18 @@
+package leetcode
+
+// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+// Input: root = [1,null,2,3]
+// Output: [1,3,2]
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+
+}
